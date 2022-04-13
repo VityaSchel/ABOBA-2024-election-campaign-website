3eb08ae1b7dbe8514d1f0f9fe17349560ee238ff
Viktor <vityaschel@utidteam.com>
2022-04-13 23:02:53 +0400

    Donations are now dynamic

--- components/Contact/donaters.tsx
+++ components/Contact/donaters.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import Link from "next/link"
-import donaters from "../../data/donaters.tsx"
+// import donaters from "../../data/donaters.tsx"
 
 class Donate extends React.Component {
   render(){
@@ -21,13 +21,39 @@ class Donate extends React.Component {
 }
 
 export default class Donaters extends React.Component {
-  render(){
-    const donates = donaters.map(don => <Donate don={don} key={don.id} />)
+  constructor(props) {
+    super(props)
+    this.state = { donaters: null }
+  }
 
+  componentDidMount() {
+    (async () => {
+      const response = await fetch('https://hloth.dev/api/getAbobaDons')
+      const dons = await response.text()
+      const donaters = dons.split('\n').filter(String).map(don => {
+        const dons = don.split(':', 4)
+        return {
+          id: dons[0],
+          name: dons[0],
+          amount: dons[1] + ' Евро',
+          message: dons[2],
+          link: dons[3] ? {
+            href: 'https://instagram.com/' + dons[3],
+            label: 'Инстаграм: @' + dons[3]
+          } : undefined
+        }
+      })
+      this.setState({ donaters })
+    })()
+  }
+
+  render(){
+    const donates = this.state.donaters?.map(don => <Donate don={don} key={don.id} />)
+    
     return (
       <div id="aboba-donaters">
         <h3 className="primary-heading">Донатеры сайта Абобы</h3>
-        {donates}
+        {donates ?? 'Загрузка...'}
       </div>
     )
   }
